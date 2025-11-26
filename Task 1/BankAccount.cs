9560f59a9200d08f1550155668d1d4089802b300
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-26 11:50:57 +0545

    task 1 completed

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public String getAccountNumber()
+        {
+            return accountNumber;
+        }
+        public double getBalance()
+        {
+            return balance;
+        }
+        private void setBalance(double value)
+        {
+            if (value < 0) value = 0;
+            balance = value;  
+        }
+        public BankAccount(string acc)
+        {
+            accountNumber = acc;
+        }
+        public void Deposit(double amount)
+        {
+            if (amount <= 0) return;
+            setBalance(getBalance() + amount);
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0 || amount > getBalance()) return;
+           setBalance(getBalance() - amount);
+        }
+
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount("utkristacc123");
+
+            Console.WriteLine("Account: " + b.getAccountNumber());
+
+            b.Deposit(500);
+            b.Withdraw(200);
+
+            Console.WriteLine("Balance: " + b.getBalance());
+        }
+    }
+}
