b895113a29c2d6fb5a5f78fb6dcc932ad9f7d5fb
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-26 20:38:03 +0545

    task 3 completed

---
+++ Task 3/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Printer
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine($"String message: {message}");
+        }
+
+        public void Print(int number)
+        {
+            Console.WriteLine($"Integer number: {number}");
+        }
+
+        public void Print(string message, int count)
+        {
+            Console.WriteLine($"Repeating '{message}' {count} times:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"- {message}");
+            }
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Printer myPrinter = new Printer();
+                myPrinter.Print("Hello, Myself!");
+                myPrinter.Print(33);
+                myPrinter.Print("C# is pretty good", 3);
+               
+           
+        }
+    }
+}
